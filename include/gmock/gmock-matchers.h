e17ec299922defa6ec8a7522dc678be9e9f24a5a
pleroy <phl+personal@google.com>
2015-07-22 21:46:42 +0200

    And the compiler was vanquished!

--- include/gmock/gmock-matchers.h
+++ include/gmock/gmock-matchers.h
@@ -527,7 +527,11 @@ class MatcherCastImpl {
     // Even if T has an implicit constructor from M, it won't be called because
     // creating Matcher<T> would require a chain of two user-defined conversions
     // (first to create T from M and then to create Matcher<T> from T).
+#if defined(_MSC_VER) && (_MSC_VER >= 1900)
+    return ImplicitCast_<Matcher<T>>(polymorphic_matcher_or_value);
+#else
     return polymorphic_matcher_or_value;
+#endif
   }
 };
 
